5e44d26fe29eedf5adc62d009de34e0dedb11109
Haithem Mihoubi <64318421+haithemmihoubi@users.noreply.github.com>
2022-08-12 10:30:49 +0100

    props and jsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <div className="App">
+            <User name={"haithem"} age={22} email={"haithem@gmail.com"} />
+            <User name={"mohammed"} age={22} numy={568} />
+        </div>
+    );
+}
+
+
+// eslint-disable-next-line no-unused-vars
+const User = (props) => {
+    return (
+        <div>
+            <h2>{props.name}</h2>
+            <h2>{props.age}</h2>
+            <h2>{props.email}</h2>
+            <h2>{props.numy}</h2>
+
+        </div>
+    );
 }
 
 export default App;
