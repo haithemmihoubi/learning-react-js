cc1e6607e813d5fcf1c909bc6bcb34cc61357064
Haithem Mihoubi <64318421+haithemmihoubi@users.noreply.github.com>
2022-08-12 10:55:55 +0100

    props and jsx create component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
+import {render} from "react-dom";
 
 function App() {
 
     return (
         <div className="App">
             <User name={"haithem"} age={22} email={"haithem@gmail.com"} />
-            <User name={"mohammed"} age={22} numy={568} />
+            <Job title={"Web Developer"} company={"Google"} location={"Paris"} description={"Web Developer"} />
+            <Job title={"Web Designer"} company={"Facebook"} location={"New York"}  />
         </div>
     );
 }
-
 
 // eslint-disable-next-line no-unused-vars
 const User = (props) => {
@@ -19,9 +20,21 @@ const User = (props) => {
             <h2>{props.age}</h2>
             <h2>{props.email}</h2>
             <h2>{props.numy}</h2>
-
         </div>
     );
 }
+
+const Job = (props) => {
+  return (
+    <div>
+        <h2>{props.title}</h2>
+        <h2>{props.company}</h2>
+        <h2>{props.location}</h2>
+        <h2>{props.description}</h2>
+    </div>
+    );
+}
+
+
 
 export default App;
